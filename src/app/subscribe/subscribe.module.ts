8981192e816e9c822f43b2d0e63e8b49112f10a4
yoanesrn <yoanesrn@gmail.com>
2024-07-03 15:48:40 +0700

    add customer subscribe

--- src/app/subscribe/subscribe.module.ts
+++ src/app/subscribe/subscribe.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { ClinicSubscriberSatuSehat } from './clinic/clinic.subscriber';
+import { CustomerSubscriberSatuSehat } from './customer/customer.subscriber';
 import { InteractionSubscriberSatuSehat } from './interaction/interaction.subscriber';
 import { RoomSubscriberSatuSehat } from './room/room.subscriber';
 import { SubscribeService } from './subscribe.service';
 
 @Module({
   providers: [
+    CustomerSubscriberSatuSehat,
     ClinicSubscriberSatuSehat,
     RoomSubscriberSatuSehat,
     SubscribeService,
