e25095fe4af60348ab68af9ebdeff39ead35d4e7
yoanesrn <yoanesrn@gmail.com>
2024-07-15 09:37:51 +0700

    load staff subscriber

--- src/app/subscribe/subscribe.module.ts
+++ src/app/subscribe/subscribe.module.ts
@@ -4,10 +4,12 @@ import { ClinicSubscriberSatuSehat } from './clinic/clinic.subscriber';
 import { CustomerSubscriberSatuSehat } from './customer/customer.subscriber';
 import { InteractionSubscriberSatuSehat } from './interaction/interaction.subscriber';
 import { RoomSubscriberSatuSehat } from './room/room.subscriber';
+import { StaffSubscriberSatuSehat } from './staff/staff.subscriber';
 import { SubscribeService } from './subscribe.service';
 
 @Module({
   providers: [
+    StaffSubscriberSatuSehat,
     CustomerSubscriberSatuSehat,
     ClinicSubscriberSatuSehat,
     RoomSubscriberSatuSehat,
